ef03d37466a8f9204744080b972b7d8e9f706532
Antgst <ant.gousset@gmail.com>
2025-12-15 11:48:50 +0100

    fix: fix the printallav.c file

---
+++ TESTS/arguments/printallav.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main: check the code
+ * @ac: the number of arguments
+ * @av: the array where arguments are stored
+ * 
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	unsigned int i;
+
+	for (i = 0; av[i] != NULL; i++)
+	{
+		printf("%s\n", av[i]);
+	}
+	return (0);
+}
