4bd7e685fbf74c85af0a2939b93a85555379cd00
Antgst <ant.gousset@gmail.com>
2025-12-16 09:27:21 +0100

    fix: rename the compiled filed for the 2

---
+++ exercices/2-commande_line_to_av.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * 
+ * 
+ * 
+ */
+
+int main(void)
+{
+    char str[] = "Split this string";
+    char *split;
+
+    if (str != NULL)
+    split = strtok(str, " ");
+
+    while (split != NULL)
+    {
+        printf("%s\n", split);
+        split = strtok(NULL, " ");
+    }
+
+    return (0);
+
+}
