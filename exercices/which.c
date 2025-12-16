a49ec7adf034968e4dda85a3ffa33b77ca17e8f3
Antgst <ant.gousset@gmail.com>
2025-12-16 15:29:53 +0100

    add: add which.c file

---
+++ exercices/which.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/**
+ * main - Mimics the behavior of the which command
+ * @ac: The number of arguments
+ * @av: The array where the arguments are stored
+ * 
+ * Return: Always 0
+ */
+
