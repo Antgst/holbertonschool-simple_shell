7481496ab609d2522b5c6e182d608af12cc6d8ee
Antgst <ant.gousset@gmail.com>
2025-12-16 14:34:13 +0100

    add: add&fix ssh and compile it

--- exercices/super_simple_shell.c
+++ exercices/super_simple_shell.c
@@ -16,13 +16,44 @@ int main(void)
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
+	int status;
+	pid_t pid;
 
-	printf("supersimpleshell$ ");
-	read = getline(&line, &len, stdin);
-	
-	if (read == -1)
+	while(1)
 	{
-		free(line);
-		return (0);
+		printf("#cisfun$ ");
+		read = getline(&line, &len, stdin);
+		
+		if (read == -1)
+		{
+			printf("\n");
+			break;
+		}
+
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("Error:");
+			continue;
+		}
+
+		if (pid == 0)
+		{
+			char *argv[] = {line, NULL};
+			execve(argv[0], argv, NULL);
+			perror("Execve:");
+			return (0);
+		}
+
+		else
+		{
+			wait(&status);
+		}
 	}
+	free(line);
+	return(0);
 }
