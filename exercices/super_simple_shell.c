f24db36ad61029d5b1bc72f12f67a0fc2cc03ec2
Antgst <ant.gousset@gmail.com>
2025-12-16 13:07:07 +0100

    add: super_simple_shell.c and compile it

--- exercices/super_simple_shell.c
+++ exercices/super_simple_shell.c
@@ -20,23 +20,15 @@ int main(void)
 	while (1)
 	{
 		printf("supersimpleshell$ ");
-		fflush(stdout);
-
 		read = getline(&line, &len, stdin);
+		
 		if (read == -1)
 		{
-			if (feof(stdin))
-			{
-				printf("\n");
-				break;
-			}
-			free(line);
-			return (-1);
+			printf("\n");
+			break;
 		}
 		
-		printf("%s", line);
+		free(line);
+		return (-1);
 	}
-
-	free(line);
-	return (0);
 }
