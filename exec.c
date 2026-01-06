745e2925e21ae7e79f33ea88ccfc2ce0767e2863
Ausaryu <yonas.houriez@gmail.com>
2026-01-06 20:02:14 +0100

    fix compillation issue

--- exec.c
+++ exec.c
@@ -47,9 +47,9 @@ int exec(char **argv, const char *sname, ssize_t line)
 	if (WIFEXITED(status))
 		code = WEXITSTATUS(status);
 	 else if (WIFSIGNALED(status))
-        code = 128 + WTERMSIG(status);
-    else
-        code = 1;
+		code = 128 + WTERMSIG(status);
+	else
+		code = 1;
 
 	return (code);
 }
